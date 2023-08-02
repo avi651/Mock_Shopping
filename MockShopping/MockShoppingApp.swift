c664cc7e6ba5e3d98955e1b11f436b2f274e1d58
= <kumaravi651@gmail.com>
2023-08-02 19:28:47 +0530

    Initial Commit

---
+++ MockShopping/MockShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockShoppingApp.swift
+//  MockShopping
+//
+//  Created by Avinash Kumar on 02/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MockShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
